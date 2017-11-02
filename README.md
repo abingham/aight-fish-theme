24cd22ca6405fb92fe636b33436d898aec96c6bc
Austin Bingham <austin.bingham@gmail.com>
2017-11-02 09:34:04 +0100

    Fiat lux!

---
+++ README.md
@@ -0,0 +1,22 @@
+# Aight
+
+A theme for fish that puts most stuff on the right.
+
+## Installation
+
+```fish
+omf update
+omf install aight
+```
+
+## Features
+
+* Stuff goes on the right, out of your way.
+
+## Contributing
+
+Just fork and make PRs.
+
+# License
+
+[MIT](https://opensource.org/licenses/MIT) © Austin Bingham 2017
---
+++ fish_prompt.fish
@@ -0,0 +1,88 @@
+function fish_prompt
+
+    # Keep the command executed status
+    set --local last_status $status
+
+    show_path
+    show_status $last_status
+end
+
+
+function show_path
+
+    set_color blue
+    echo -en "["
+
+    set_color yellow
+    echo -en (prompt_pwd)
+
+    set_color blue
+    echo -en "] "
+end
+
+
+function show_status -a last_status
+
+    set --local current_color "FFF"
+
+    if [ $last_status -ne 0 ]
+        set current_color red
+    end
+
+    set_color $current_color
+    echo -en ": "
+    set_color normal
+end
+
+
+function fish_right_prompt
+
+    # A dark grey
+    set --local dark_grey 555
+
+    set_color $dark_grey
+
+    show_virtualenv_name
+    show_git_info
+    echo -en (date +%H:%M:%S)
+
+    set_color normal
+end
+
+
+function show_virtualenv_name
+
+    if set -q VIRTUAL_ENV
+        echo -en "["(basename "$VIRTUAL_ENV")"] "
+    end
+end
+
+
+function show_git_info
+
+    set --local LIMBO /dev/null
+    set --local git_status (git status --porcelain 2> $LIMBO)
+    set --local dirty ""
+
+    [ $status -eq 128 ]; and return  # Not a repository? Nothing to do
+
+    # If there is modifications, set repository dirty to '*'
+    if not [ -z (echo "$git_status" | grep -e '^ M') ]
+        set dirty "*"
+    end
+
+    # If there is new or deleted files, add  '+' to dirty
+    if not [ -z (echo "$git_status" | grep -e '^[MDA]') ]
+        set dirty "$dirty+"
+    end
+
+    # If there is stashed modifications on repository, add '^' to dirty
+    if not [ -z (git stash list) ]
+        set dirty "$dirty^"
+    end
+
+    # Prints git repository status
+    echo -en "("
+    echo -en (git_branch_name)$dirty
+    echo -en ") "
+end
